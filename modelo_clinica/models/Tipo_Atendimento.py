6eeee7f4a071459d247057166e9e63677de5b247
taylorminioffice-alt <taylorminioffice@gmail.com>
2026-05-21 12:32:16 -0300

    criação das entidades (models)
    entidades criadas: Papel (abstract)
    PapelProfissional
    PapelPaciente
    MetodoPagamento (abstract)
    MetodoDinheiro
    MetodoPix
    MetodoCartao
    TipoAtendimento

---
+++ modelo_clinica/models/Tipo_Atendimento.py
@@ -0,0 +1,22 @@
+
+class TipoAtendimento:
+
+    def __init__(self, nome: str, descricao: str):
+        self.__nome = nome
+        self.__descricao = descricao
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def descricao(self):
+        return self.__descricao
+
+    @descricao.setter
+    def descricao(self, descricao):
+        self.__descricao = descricao
