0ffbc51f251903450c080995da33874ebedf57a2
taylorminioffice-alt <taylorminioffice@gmail.com>
2026-07-10 20:07:39 -0300

    Reconstrução da tela_pagamento.py

--- src/view/tela_pagamento.py
+++ src/view/tela_pagamento.py
@@ -3,36 +3,40 @@ import FreeSimpleGUI as sg
 
 class TelaPagamento:
 
-    def __init__(self):
-        self.__window = None
-        self.init_components()
+def init(self):
 
-    def init_components(self):
-        sg.ChangeLookAndFeel("DarkTeal4")
+pass
 
-        layout = [
-            [sg.Text("Cadastro de Pagamento", font=("Helvetica", 16))],
 
-            [sg.Text("Código:"), sg.Input(key="codigo")],
+def init_components(self):  
+    sg.ChangeLookAndFeel("DarkTeal4")  
 
-            [sg.Text("Código do Atendimento:"), sg.Input(key="atendimento")],
+    layout = [  
+        [sg.Text("SisClinicas", font=("Helvetica", 18), justification="center")],  
+        [sg.Text("Escolha a opção desejada:")],  
+        [sg.Radio("1 - Cadastro de Pessoas (Pacientes / Médicos)", "RD1", key=1)],  
+        [sg.Radio("2 - Gerenciar Atendimentos (Consultas / Procedimentos)", "RD1", key=2)],  
+        [sg.Radio("3 - Relatórios e Estatísticas", "RD1", key=3)],  
+        [sg.Radio("4 - Cadastro de Clinicas", "RD1", key=4)],  
+        [sg.Radio("5 - Cadastro de Tipos de Atendimento", "RD1", key=5)],  
+        [sg.Radio("0 - Finalizar o Sistema", "RD1", key=0)],  
+        [sg.Button("Confirmar"), sg.Cancel('Cancelar')]  
+    ]  
 
-            [sg.Text("Valor:"), sg.Input(key="valor")],
+def mensagem(self, msg):  
+    sg.popup(msg)  
+  
+def close(self):  
+    self.__window.Close()  
 
-            [sg.Text("Forma de Pagamento:"),
-             sg.Combo(
-                 ["Dinheiro", "Pix", "Cartão de Débito", "Cartão de Crédito"],
-                 key="forma",
-                 readonly=True
-             )],
+def open(self):  
+    evento, valores = self.__window.read()  
+    return evento, valores  
 
-            [sg.Text("Data:"), sg.Input(key="data")],
 
-            [sg.Button("Salvar"),
-             sg.Button("Cancelar")]
-        ]
 
-        self.__window = sg.Window("Pagamento", layout)
+Se não estiver, me mande a codificação completa dela
+
 
     def open(self):
         evento, valores = self.__window.read()
